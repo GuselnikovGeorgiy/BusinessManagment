8bc934086780216cb136cb5b1358534c0a0b1133
Jorique <guselnikovgeorgiy@gmail.com>
2025-05-16 02:59:11 +0300

    added company model

---
+++ app/models/companies.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+from app.models.users import User
+from app.models.departments import Department
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    employees: Mapped[list["User"]] = relationship(back_populates="company")
+    departments: Mapped[list["Department"]] = relationship("Department", back_populates="company")
